3964102506ce811ad76cd050c3c8a10b16ec32bd
Mateus Davi Simon <hlah_@hotmail.com>
2019-09-16 19:54:03 -0300

    Added GLFW and GLAD.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 
-#include "def.hpp"
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "The value of pi is " << PI << std::endl;
+    GLFWwindow* window;
 
+
+    if(!glfwInit()) {
+        std::cerr << "Could not load Open GLFW." << std::endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(800, 800, "Hello World", NULL, NULL);
+    if (!window) 
+    {
+        std::cerr << "Could not create window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGL()) {
+        std::cerr << "Could not load Open GL." << std::endl;
+        return -1;
+    }
+
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
